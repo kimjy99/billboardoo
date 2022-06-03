7d3992fd379d0a6ca1a2d404a64b516b240d3b23
kimjy99 <99edward@snu.ac.kr>
2022-06-04 01:36:42 +0900

    Add build files

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ed9e814f761d4fe2c2e30b947a569b40",
-"/": "ed9e814f761d4fe2c2e30b947a569b40",
-"main.dart.js": "e2308e34a91597af85a927a42e223e71",
+"index.html": "ac24b25b483789fa34a0a9b910307046",
+"/": "ac24b25b483789fa34a0a9b910307046",
+"main.dart.js": "190c99241476e4c3e9f952c79fa091c5",
 "manifest.json": "b3cfb42d18d820489d528c622bf91646",
 "version.json": "099b8e565fd9b3044dff5b3034782f5f"
 };
